e11978971ee6116051e7d5e7e66f5cf5be5cf8c3
Mona Lisa <anthonyrichardo080@gmail.com>
2025-08-28 14:42:39 +0100

    adding some additional features

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const days = document.getElementById("day-input");
+const month = document.getElementById("month-input");
+const year = document.getElementById("year-input");
 const calculateButton = document.getElementById("age-calculate");
 const inputs = document.querySelectorAll(".input")
 
 function calculateage(){
-    inputs.forEach(input => {
-        if(input.value == ""){
+   
+}
+
+inputs.forEach(input => input.addEventListener('input', () => {
+     if(input.value == ""){
           input.parentElement.classList.remove("text-grey500");
           input.parentElement.classList.add("text-red400");
           input.classList.remove("border-grey200");
           input.classList.add("border-red400");
+          input.classList.remove("focus:outline-purple500")
+          input.classList.add("focus:outline-red400")
           input.nextElementSibling.classList.remove("hidden");
+        } else {
+            input.parentElement.classList.remove("text-red400");
+          input.parentElement.classList.add("text-grey500");
+          input.classList.remove("border-red400");
+          input.classList.add("border-grey200");
+          input.classList.remove("focus:outline-red400")
+          input.classList.add("focus:outline-purple500");
+          input.nextElementSibling.classList.add("hidden");
         }
-    })
 
-    console.log(days.value)
-}
+        console.log(input)
+}))
+
 
 
 
